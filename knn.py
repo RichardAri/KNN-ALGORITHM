bc7349e99ceb1d52673e7b03c9892119ce1e8c1e
RichardAri <richardaripari@gmail.com>
2024-05-02 14:17:45 -0500

    Se añadio precision de vecinos

--- knn.py
+++ knn.py
@@ -38,20 +38,30 @@ model.fit(x_train, y_train)
 #? prediccion sobre las 3 muestras
 
 predictions = model.predict(X_test)
-print("Predicciones:", predictions)
-
+print("Predicciones:", predictions) 
+#! Predicciones: [1, 2, 0]
+#* significa que el modelo predijo las siguientes clases para las muestras de prueba:
+#* la primera muestra se predijo como clase 1 => Clase 1: Iris-versicolor
+#* la segunda muestra se predijo como clase 2 => Clase 2: Iris-virginica
+#* la tercera muestra se predijo como clase 0 => Clase 0: Iris-setosa
 
 #? porcentaje de acierto con un vecino 
 
 accuracy = model.score(X_test, y_test) #* Precision
 print("Porcentaje de acierto con 1 vecino:", accuracy)
+#! Porcentaje de acierto con 1 vecino: 0.6666666666666666
+#* es el porcentaje de muestras de prueba que el modelo clasifico correctamente 
+#* cuando se usó 1 vecino, el modelo acerto en aproximadamente el 
+#* 66.67% de las veces cuando solo consideró a un vecino más cercano para clasificar una muestra
 
 #? porcentaje de acierto para 3 vecinos
 
-#! nuevo modelo con 3 vecinos (neighbors)
+# modelo con 3 vecinos (neighbors)
 model_3_neighbors = KNeighborsClassifier(n_neighbors=3)
 model_3_neighbors.fit(x_train, y_train) #! entrenamos el modelo
 
-#! Calculamos el porcentaje de acierto con el nuevo modelo
+# porcentaje de precision con el nuevo modelo
 accuracy_3_neighbors = model_3_neighbors.score(X_test, y_test)
 print("Porcentaje de acierto con 3 vecinos:", accuracy_3_neighbors)
+#! Porcentaje de acierto con 3 vecinos: 1.0 (o 100%
+#* el modelo clasificó correctamente el 100% cuando se usaron 3 vecinos
